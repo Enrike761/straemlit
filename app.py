b7e83d344699da7d9e0bc76ce63ffac81be84988
Enrike761 <enrikediazmilans@gmail.com>
2024-11-16 14:20:58 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+favourtite_colour = st.text_input("favourtite_colour:")
+favorite_animal = st.text_input ("favorite animal:")
+you_number = st.number_input("your lucky number:")
+superpower = st.selectbox("choose your superpower:", ["fly", "teleport","jumper"])
+superhero_name = st.write(f"{favourtite_colour} {favorite_animal} of {you_number}") ❤️
